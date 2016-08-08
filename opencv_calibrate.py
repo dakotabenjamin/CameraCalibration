595e38790704de3178080f7f1d46e27d0780aa19
Dakota Benjamin <dakota.benjamin@gmail.com>
2016-08-08 13:12:40 -0400

    Initial commit

---
+++ opencv_calibrate.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+'''
+camera calibration for distorted images with chess board samples
+reads distorted images, calculates the calibration and write undistorted images
+usage:
+    calibrate.py [--debug <output path>] [--square_size] [<image mask>]
+default values:
+    --debug:    .sample/output/
+    --square_size: 1.0
+    <image mask> defaults to .sample/chessboard/*.jpg
+
+
+COPYRIGHT GOES TO OPENCV, this is not mine.
+'''
+
+# Python 2/3 compatibility
+from __future__ import print_function
+
+import numpy as np
+import cv2
+
+# local modules
+from common import splitfn
+
+# built-in modules
+import os
+
+if __name__ == '__main__':
+    import sys
+    import getopt
+    from glob import glob
+
+    args, img_mask = getopt.getopt(sys.argv[1:], '', ['debug=', 'square_size='])
+    args = dict(args)
+    if not img_mask:
+        img_mask = './sample/chessboard/*.jpg'  # default
+    else:
+        img_mask = img_mask[0]
+    proj_root = img_mask.split('*')[0]
+    args.setdefault('--debug', proj_root)
+    args.setdefault('--square_size', 1.0)
+
+
+    img_names = glob(img_mask)
+    debug_dir = args.get('--debug')
+    if not os.path.isdir(debug_dir):
+        os.mkdir(debug_dir)
+    square_size = float(args.get('--square_size'))
+
+    pattern_size = (9, 6)
+    pattern_points = np.zeros((np.prod(pattern_size), 3), np.float32)
+    pattern_points[:, :2] = np.indices(pattern_size).T.reshape(-1, 2)
+    pattern_points *= square_size
+
+    obj_points = []
+    img_points = []
+    h, w = 0, 0
+    img_names_undistort = []
+    for fn in img_names:
+        print('processing %s... ' % fn, end='')
+        img = cv2.imread(fn, 0)
+        if img is None:
+            print("Failed to load", fn)
+            continue
+
+        h, w = img.shape[:2]
+        found, corners = cv2.findChessboardCorners(img, pattern_size)
+        if found:
+            term = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_COUNT, 30, 0.1)
+            cv2.cornerSubPix(img, corners, (5, 5), (-1, -1), term)
+
+        if debug_dir:
+            vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+            cv2.drawChessboardCorners(vis, pattern_size, corners, found)
+            path, name, ext = splitfn(fn)
+            outfile = debug_dir + name + '_chess.png'
+            cv2.imwrite(outfile, vis)
+            if found:
+                img_names_undistort.append(outfile)
+
+        if not found:
+            print('chessboard not found')
+            continue
+
+        img_points.append(corners.reshape(-1, 2))
+        obj_points.append(pattern_points)
+
+        print('ok')
+
+    # calculate camera distortion
+    rms, camera_matrix, dist_coefs, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, (w, h), None, None)
+
+    print("\nRMS:", rms)
+    print("camera matrix:\n", camera_matrix)
+    # print("matrix: \n", type(camera_matrix))
+    print("distortion coefficients: ", dist_coefs.ravel())
+
+    # write to matrix to be used as input
+    with open(debug_dir + "matrix.txt", "w") as matf:
+        camera_matrix.reshape((3, 3))
+        np.savetxt(matf, (camera_matrix[0], camera_matrix[1], camera_matrix[2]), fmt='%-12.8f')
+
+    with open(debug_dir + "distortion.txt", "w") as distf:
+        np.savetxt(distf, dist_coefs.ravel(), fmt='%.12f')
+
+    # undistort the image with the calibration
+    print('')
+    for img_found in img_names_undistort:
+        img = cv2.imread(img_found)
+
+        h,  w = img.shape[:2]
+        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, dist_coefs, (w, h), 1, (w, h))
+
+        dst = cv2.undistort(img, camera_matrix, dist_coefs, None, newcameramtx)
+
+        # crop and save the image
+        x, y, w, h = roi
+        dst = dst[y:y+h, x:x+w]
+        outfile = img_found + '_undistorted.png'
+        print('Undistorted image written to: %s' % outfile)
+        cv2.imwrite(outfile, dst)
+
+    cv2.destroyAllWindows()
---
+++ undistort.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+"""
+image undistortion for cameras with calibration information.
+reads distorted images and their calibration matrix and distortion coefficients
+and writes undistorted photos
+usage:
+    undistort.py --matrix <matrix path> --distortion <distortion path> <image path>
+
+Code forked from Jan Erik Solem's blog:
+http://www.janeriksolem.net/2014/05/how-to-calibrate-camera-with-opencv-and.html
+"""
+
+import numpy as np
+import cv2
+import glob
+import os
+import logging
+import sys
+import getopt
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
+
+if __name__ == '__main__':
+    args, img_path = getopt.getopt(sys.argv[1:], '', ['matrix=', 'distortion='])
+    args = dict(args)
+    args.setdefault('--matrix', os.path.abspath('./sample/output/matrix.txt'))
+    args.setdefault('--distortion', os.path.abspath('./sample/output/distortion.txt'))
+    if not img_path:
+        img_path = os.path.abspath('./sample/img/*.jpg')  # default
+    else:
+        img_path = img_path[0]
+
+    matrix_path = args.get('--matrix')
+    distortion_path = args.get('--distortion')
+    out_path = '\\'.join(img_path.split('\\')[:-1]) + '\\out\\'
+
+    # load parameters to np arrays
+    K = np.loadtxt(matrix_path)
+    d = np.loadtxt(distortion_path)
+    # d[2:] = 0  # We only need first two values
+
+    print("Matrix: \n" + str(K))
+    print("Distortion: " + str(d))
+
+    # copy parameters to arrays
+    # K = np.array([[1743.23312, 0, 2071.06177], [0, 1741.57626, 1476.48298], [0, 0, 1]])
+    # d = np.array([-0.307412748, 0.300929683, 0, 0, 0])  # just use first two terms (no translation)
+
+    if not os.path.isdir(out_path):
+        os.mkdir(out_path)
+
+    logging.debug("Starting loop")
+    logging.debug("writing images to {0})".format(out_path))
+    for image in glob.glob(img_path):
+        # logging.debug("var %s", image)
+        imgname = image.split("\\")[-1]
+        logging.debug("Undistorting %s . . . ", imgname)
+        # read one of your images
+        img = cv2.imread(image)
+        h, w = img.shape[:2]
+
+        # un-distort
+        newcamera, roi = cv2.getOptimalNewCameraMatrix(K, d, (w, h), 0)
+        newimg = cv2.undistort(img, K, d, None, newcamera)
+
+        # TODO Write exif: camera maker, camera model, focal length?, GPS to new file
+        # cv2.imwrite("original.jpg", img)
+        newimg_path = out_path + imgname
+        cv2.imwrite(newimg_path, newimg)
+
+        # Only works on linux
+        command = "exiftool -TagsFromFile {0} -all:all {1}".format(image, newimg_path)
